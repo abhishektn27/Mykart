8542bcb39c99a55231ae89fca90896849178800b
abhishektn27 <148741999+abhishektn27@users.noreply.github.com>
2024-03-26 22:33:06 +0530

    prodect_disply

--- Mykart/products/views.py
+++ Mykart/products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -12,7 +14,16 @@ def list_products(request):
     :param request:
     :return:
     """
-    return render(request,'products.html')
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    product_list=Product.objects.all()
+    product_paginator=Paginator(product_list,2)
+    product_list=product_paginator.get_page(page)
+    context={'Products':product_list}
+    return render(request,'products.html',context)
 
-def detail_product(request):
-    return render(request,'product_detail.html')
+def detail_product(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request,'product_detail.html',context)
